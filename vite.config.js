a1e828c082925016869d3709e3f8ea748f25cee0
pricila acsama <pricilaacsama12@gmail.com>
2025-10-15 03:40:39 -0300

    preparando deploy a github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/steezy_pagina/', // <- debe coincidir con el nombre del repo
   plugins: [react()],
 })
